febcbf959265764fe282b15ec52c7eb75df94280
ChesterBu <buyuxing@meituan.com>
2020-04-15 10:09:39 +0800

    init

---
+++ demo/test.js
@@ -0,0 +1,19 @@
+/* global Vue */
+
+new Vue({    
+    el:".a",        
+    name:"A",    
+    components:{        
+        testb:{            
+            props:{                
+                childName:""
+            },            
+        template: '<p>父组件传入的 props 的值 {{childName}}</p>',
+        }
+    },
+    data(){        
+        return {            
+            parentName:"我是父组件"
+        }
+    },
+})
